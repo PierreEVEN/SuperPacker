492a53676bc6cb99c8ad3ba66fac25ba407215da
PierreEVEN <evenpierre@orange.fr>
2022-11-30 17:51:58 +0100

    initial commit

--- Sources/SuperPacker/Public/ApiInteface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <filesystem>
-#include <optional>
-
-namespace SuperPacker
-{
-	std::optional<std::filesystem::path> pick_file(const std::string& destination, std::vector<char> formats);
-	std::optional<std::filesystem::path> save_file(std::vector<char> formats);
-}
